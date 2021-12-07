f49b5d440575c4d8d752ccc256a34965aaa73317
Scott Davies <scott@protostream.io>
2021-12-07 13:40:05 +0000

    Moved char methods into their own extension classes. Internal classes marked when used but not directly unit tested.

---
+++ io.protostream.extensions/io.protostream.extensions/CharExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace io.protostream.extensions
+{
+    public static class CharExtensions
+    {
+
+        /// <summary>
+        /// NOT UNIT TESTED .: SET TO INTERNAL
+        /// Returns the integer value of a hex character.
+        /// Inspired by: https://stackoverflow.com/questions/321370/how-can-i-convert-a-hex-string-to-a-byte-array
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns></returns>
+        internal static int GetHexVal(this char hex)
+        {
+            if (!hex.IsHex())
+                throw new Exception("Strings contains non-hex characters.");
+
+            int val = (int)hex;
+            //For uppercase A-F letters:
+            //return val - (val < 58 ? 48 : 55);
+            //For lowercase a-f letters:
+            //return val - (val < 58 ? 48 : 87);
+            //Or the two combined, but a bit slower:
+            return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+        }
+
+        /// <summary>
+        /// NOT UNIT TESTED .: SET TO INTERNAL
+        /// Returns true if the character provided is hex.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        internal static bool IsHex(this char c)
+        {
+            return (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') || (c >= '0' && c <= '9');
+        }
+    }
+}
--- io.protostream.extensions/io.protostream.extensions/StringExtensions.cs
+++ io.protostream.extensions/io.protostream.extensions/StringExtensions.cs
@@ -52,40 +52,10 @@ namespace io.protostream.extensions
 
             for (int i = 0; i < hex.Length >> 1; ++i)
             {
-                arr[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+                arr[i] = (byte)(((hex[i << 1]).GetHexVal() << 4) + (hex[(i << 1) + 1]).GetHexVal());
             }
 
             return arr;
-        }
-
-        /// <summary>
-        /// Returns the integer value of a hex character.
-        /// Inspired by: https://stackoverflow.com/questions/321370/how-can-i-convert-a-hex-string-to-a-byte-array
-        /// </summary>
-        /// <param name="hex"></param>
-        /// <returns></returns>
-        private static int GetHexVal(char hex)
-        {
-            if (!hex.IsHex())
-                throw new Exception("Strings contains non-hex characters.");
-
-            int val = (int)hex;
-            //For uppercase A-F letters:
-            //return val - (val < 58 ? 48 : 55);
-            //For lowercase a-f letters:
-            //return val - (val < 58 ? 48 : 87);
-            //Or the two combined, but a bit slower:
-            return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
-        }
-
-        /// <summary>
-        /// Returns true if the character provided is hex.
-        /// </summary>
-        /// <param name="c"></param>
-        /// <returns></returns>
-        private static bool IsHex(this char c)
-        {
-            return (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') || (c >= '0' && c <= '9');
         }
     }
 }
