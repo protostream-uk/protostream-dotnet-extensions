840e01731dceae9c862bf6b846610dc0f23f1b46
Scott Davies <scott@protostream.io>
2021-12-07 14:10:09 +0000

    Added stream to base64 and stream to string. Unit tests required.

---
+++ io.protostream.extensions/io.protostream.extensions/StreamExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.protostream.extensions
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Converts a stream into a base64 string
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static async Task<string> ToBase64String(this Stream stream)
+        {
+            using MemoryStream ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            byte[] requestBody = ms.ToArray();
+            return Convert.ToBase64String(requestBody);
+        }
+
+        /// <summary>
+        /// Converts the a stream into a UTF8 string
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static async Task<string> GetString(this Stream body)
+        {
+            using MemoryStream ms = new MemoryStream();
+            await body.CopyToAsync(ms);
+            byte[] requestBody = ms.ToArray();
+            return Encoding.UTF8.GetString(requestBody);
+        }
+    }
+}
